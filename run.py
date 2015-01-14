dfc86aad581f58a18c4f78d124d46556de7ef2da
mgmonteleone-admin <mgm@aut-aut.hr>
2015-01-14 21:13:50 +0100

    updated

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 __author__ = 'matthewgmonteleone'
-from PIL import Image
-import os
-from flask import Flask, request, redirect, url_for, make_response
-from werkzeug import secure_filename
+from flask import Flask, request, make_response
+import werkzeug
 
 UPLOAD_FOLDER = '/tmp/'
 ALLOWED_EXTENSIONS = set(['jpeg'])
@@ -14,7 +12,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/", methods=['POST'])
 def upload():
     original = request.files.get('file')
-    return make_response("hello",200)
+    #mimetype = file.content_type
+    filename = werkzeug.secure_filename(file.name)
+    return make_response(filename,200)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
